9e774a63cfa14121f09eacafb6313acafed19667
Mikhail <m.vinkler@gmail.com>
2024-10-23 18:50:35 +0200

    geocoding error logging

--- app/Components/GeocodingClient.php
+++ app/Components/GeocodingClient.php
@@ -24,29 +24,35 @@ class GeocodingClient
     * @param string $address
     * @return array|null
     */
-   public function getCoordinatesByAddress(string $address): ?array
-   {
-       try {
-           $response = $this->client->request('GET', '', [
-               'query' => [
-                   'address' => $address,
-                   'key' => $this->apiKey
-               ]
-           ]);
-   
-           $data = json_decode($response->getBody()->getContents(), true);
-   
-           if (!empty($data['results']) && $data['status'] == 'OK') {
-               return [
-                   'latitude' => $data['results'][0]['geometry']['location']['lat'],
-                   'longitude' => $data['results'][0]['geometry']['location']['lng']
-               ];
-           }
-       } catch (\Exception $e) {
-           Log::error("Error contacting Google Geocoding API: " . $e->getMessage());
-       }
-   
-       return null;
-   }
+    public function getCoordinatesByAddress(string $address): ?array
+    {
+        try {
+            $response = $this->client->request('GET', '', [
+                'query' => [
+                    'address' => $address,
+                    'key' => $this->apiKey
+                ]
+            ]);
+
+            $data = json_decode($response->getBody()->getContents(), true);
+            Log::info('Geocoding API response', $data); // Логирование ответа API
+
+            if (!empty($data['results']) && $data['status'] == 'OK') {
+                return [
+                    'latitude' => $data['results'][0]['geometry']['location']['lat'],
+                    'longitude' => $data['results'][0]['geometry']['location']['lng']
+                ];
+            } else {
+                Log::error('Geocoding API returned no results or an error', [
+                    'status' => $data['status'],
+                    'error_message' => $data['error_message'] ?? 'No error message'
+                ]);
+            }
+        } catch (\Exception $e) {
+            Log::error("Error contacting Google Geocoding API: " . $e->getMessage());
+        }
+
+        return null;
+    }
 }
 
